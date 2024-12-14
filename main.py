3bc485609a005d3dfd43615d8c0ba739cffc824f
Surabhi15132 <surabhi15132@gmail.com>
2024-12-15 01:35:01 +0530

    Fix port binding

--- main.py
+++ main.py
@@ -62,5 +62,5 @@ def index():
 
     return render_template("index.html", result=None)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
